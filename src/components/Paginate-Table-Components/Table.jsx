464b7d476941177eb4f8b5842f682490d8c52631
Krishna Parmar <krishnaparmar850@gmail.com>
2024-07-25 12:36:29 +0530

    Add Table Mapping

--- src/components/Paginate-Table-Components/Table.jsx
+++ src/components/Paginate-Table-Components/Table.jsx
@@ -1,27 +1,36 @@
 import React from "react";
+import PaginateTableData from "../../public/PaginateTableData";
 
 function Table() {
+  const [tableData, setTableData] = React.useState(PaginateTableData);
+
   return (
     <div>
-      <table style={{ border: "1px solid black" }}>
-        <thead>
+      <table style={{ width: "100%" }}>
+        <thead style={{ textAlign: "left", border: "1px solid black" }}>
           <tr>
-            <th style={{ width: "30%" }}>Title</th>
-            <th style={{ width: "50%" }}>Title</th>
-            <th style={{ width: "20%" }}>Title</th>
+            <th>Title</th>
+            <th>Start Date</th>
+            <th>End Date</th>
+            <th>Price</th>
+            <th>Validity/Expiry</th>
+            <th>Status</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td style={{ width: "30%" }}>Title</td>
-            <td style={{ width: "50%" }}>Title</td>
-            <td style={{ width: "20%" }}>Title</td>
-          </tr>
-          <tr>
-            <td style={{ width: "30%" }}>Title</td>
-            <td style={{ width: "50%" }}>Title</td>
-            <td style={{ width: "20%" }}>Title</td>
-          </tr>
+          {tableData.map((item, index) => (
+            <tr key={index}>
+              {/* <td>
+                <img src={item.imageUrl} alt={item.title} />
+              </td> */}
+              <td>{item.title}</td>
+              <td>{item.startDate}</td>
+              <td>{item.endDate}</td>
+              <td>{item.price}</td>
+              <td>{item.validityExpiry}</td>
+              <td>{item.status}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
